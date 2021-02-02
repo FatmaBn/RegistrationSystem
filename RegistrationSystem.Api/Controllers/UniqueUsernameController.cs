cdd0c8599f6a520581273d8816f4e9f2706fc27a
Fatma BEN NEJMA <fatma.bennejma24@gmail.com>
2021-02-02 23:15:45 +0100

    Add project files.

---
+++ RegistrationSystem.Api/Controllers/UniqueUsernameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RegistrationSystem.Api.Services;
+
+namespace RegistrationSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UniqueUsernameController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome de the username portion of a registration system web API";
+        }
+
+        [HttpGet("uniqueUsername")]
+        public IActionResult uniqueUsername(string text)
+        {
+            //Create Regex to test Text validation
+            Regex lettersOnly = new Regex("^[a-z,]+$");
+
+            //Text parameter should be not empty and lowercase
+            if (string.IsNullOrEmpty(text) || !lettersOnly.IsMatch(text))
+            {
+                //Return status code with message
+                return BadRequest("user names are always non empty string that contains only lowercase English letters in the ASCII range [a-z]");
+            }
+
+            UniqueUsernameService UniqueUsernameService = new UniqueUsernameService();
+            
+            List<string> result = UniqueUsernameService.getUniqueUsername(text);
+
+            //Return status code with result
+            return Ok(JsonConvert.SerializeObject(result));
+        }
+
+    }
+}
---
+++ RegistrationSystem.Api/Services/UniqueUsernameService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistrationSystem.Api.Services
+{
+    public class UniqueUsernameService
+    {
+        public List<string> getUniqueUsername(string text)
+        {
+            List<string> result = new List<string>();
+
+            //Create a dictionary that contains names as key with its occurencies as value
+            IDictionary<string, int> names = new Dictionary<string, int>();
+
+            //Split Text to table names
+            var lstnames = text.Split(",");
+            for (int i = 0; i < lstnames.Length; i++)
+            {
+                if (string.IsNullOrEmpty(lstnames[i]))
+                    continue;
+                //If name first appears
+                if (!names.ContainsKey(lstnames[i]))
+                {
+                    names.Add(lstnames[i], 0);
+                    result.Add(lstnames[i]);
+                }
+                else
+                {
+                    //Increment occurency number
+                    names[lstnames[i]] += 1;
+                    result.Add(lstnames[i] + names[lstnames[i]]);
+                }
+            }
+            return (result);
+        }
+
+    }
+
+}
